ca0c558c5a297efdc9ed5dee7418e4eb265bee81
Mehovod <makmel726@yandex.ru>
2025-04-04 12:11:13 +0300

    Init

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+
+use core::fmt::Write;
+use microbit::{self as _, hal::uarte::{Baudrate, Parity, Uarte}};
+use microbit::Board;
+
+#[entry]
+fn main() -> ! {
+    let board = Board::take().unwrap();
+
+    let mut serial = Uarte::new(
+        board.UARTE0,
+        board.uart.into(),
+        Parity::EXCLUDED,
+        Baudrate::BAUD115200,
+    );
+
+    writeln!(serial, "Hello from microbit!")?;
+
+    loop {}
+}
+
+#[panic_handler]
+fn painc_handler(_info: &core::panic::PanicInfo) -> ! {
+    /* write!(serial, "panicked: {}", panic_info.message()); */
+    loop {}
+}
