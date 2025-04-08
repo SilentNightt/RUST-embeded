6e9d5d737009a0a65b8d059df56bb98e9d4d1ebd
Stepanov Vadim <stepanov.v.b.00@mail.ru>
2025-04-08 11:34:45 +0300

    Lab_21

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 #![no_main]
 #![no_std]
 
@@ -6,6 +7,12 @@ use cortex_m_rt::entry;
 use core::fmt::Write;
 use microbit::{self as _, hal::uarte::{Baudrate, Parity, Uarte}};
 use microbit::Board;
+use microbit::pac::twim0::frequency::FREQUENCY_A;
+
+use microbit::hal::delay::Delay;
+
+use microbit::hal::Twim;
+use lsm303agr::{Lsm303agr, MagMode, MagOutputDataRate};
 
 #[entry]
 fn main() -> ! {
@@ -18,9 +25,24 @@ fn main() -> ! {
         Baudrate::BAUD115200,
     );
 
-    writeln!(serial, "Hello from microbit!");
+    let i2c = Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100);
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
 
-    loop {}
+    let mut delay = Delay::new(board.SYST);
+
+    sensor.init().unwrap();
+    sensor.set_mag_mode_and_odr(&mut delay, MagMode::HighResolution, MagOutputDataRate::Hz10).unwrap();
+
+    let Ok(mut sensor) = sensor.into_mag_continuous() else {
+        panic!("Panic!");
+    };
+
+    loop {
+        if sensor.accel_status().unwrap().xyz_new_data() {
+            let data = sensor.magnetic_field().unwrap();
+            writeln!(serial, "Field: x {} y {} z {}", data.x_nt(), data.y_nt(), data.z_nt()).ok();
+        }
+    }
 }
 
 #[panic_handler]
